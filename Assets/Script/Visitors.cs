e0e9d3cda84fdbd119cc3e390e8a1fb5f73180ca
kotet912 <kotet912@yandex.ru>
2024-06-06 22:51:53 +0300

    Добавил посетителя и функции к нему
    посетитель двигается к определенному столу, если у посетителя есть заказ он окрашен в красный цвет, если нет то окрашен в белый, кол-во заказов у посетителя выбирается в диапазоне

--- Assets/Script/Visitors.cs
+++ Assets/Script/Visitors.cs
@@ -4,17 +4,76 @@ using UnityEngine;
 
 public class Visitors : MonoBehaviour
 {
-    [SerializeField] int _speedVisitor;
-    [SerializeField] int _ordersNumberVisitor;
-    // Start is called before the first frame update
+    [SerializeField] private float _speedVisitor; // Скорость посетителя
+    [SerializeField] private int _minOrders; // Минимальное количество заказов
+    [SerializeField] private int _maxOrders; // Максимальное количество заказов
+    [SerializeField] private Transform _tableTransform; // Трансформ стола
+    [SerializeField] private Color _orderColor = Color.red; // Цвет для обозначения наличия заказа
+    [SerializeField] private Color _defaultColor = Color.white; // Цвет по умолчанию
+
+    private int _ordersNumberVisitor; // Количество заказов у посетителя
+    private Vector3 _targetPosition;
+    private bool _hasReachedTable = false; // Булева переменная для отслеживания достижения стола
+    private SpriteRenderer _spriteRenderer; // Ссылка на компонент SpriteRenderer
+
     void Start()
     {
-        
+        // Инициализация количества заказов случайным образом в заданном диапазоне
+        _ordersNumberVisitor = Random.Range(_minOrders, _maxOrders + 1);
+
+        // Получаем компонент SpriteRenderer
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Проверка и установка цели
+        if (_tableTransform != null)
+        {
+            _targetPosition = _tableTransform.position;
+        }
+        else
+        {
+            Debug.LogError("Table Transform is not assigned in the inspector.");
+        }
+
+        // Устанавливаем цвет в зависимости от наличия заказа
+        UpdateVisitorAppearance();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Двигаем посетителя в сторону стола, если он еще не достиг его
+        if (!_hasReachedTable)
+        {
+            MoveTowardsTable();
+        }
+    }
+
+    private void MoveTowardsTable()
+    {
+        if (_tableTransform != null)
+        {
+            // Рассчитываем направление и перемещаем объект
+            Vector3 direction = (_targetPosition - transform.position).normalized;
+            transform.position += direction * _speedVisitor * Time.deltaTime;
+
+            // Проверяем, достиг ли объект стола
+            if (Vector3.Distance(transform.position, _targetPosition) < 0.5f)
+            {
+                Debug.Log("Visitor has reached the table.");
+                _hasReachedTable = true; // Устанавливаем флаг, что посетитель достиг стола
+            }
+        }
+    }
+
+    private void UpdateVisitorAppearance()
+    {
+        // Если у посетителя есть заказы, меняем цвет на красный
+        if (_ordersNumberVisitor > 0)
+        {
+            _spriteRenderer.color = _orderColor;
+        }
+        else
+        {
+            _spriteRenderer.color = _defaultColor;
+        }
     }
 }
